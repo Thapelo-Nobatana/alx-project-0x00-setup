0e3374751cf715544139ff701767b303a41a2d16
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-07-20 00:03:46 +0200

    fix: update Home component to match required format

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -1,20 +1,9 @@
 
-// import { Geist, Geist_Mono } from "next/font/google";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
 
 const Home: React.FC = ()  => {
   return (
     <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
-      <h1>Airbnb Application clone system</h1>
+      <h1>Airbnb Application Clone system</h1>
       <button className="border px-3 py-1 text-lg mt-3 bg-blue-500 text-white rounded-full">Get Started</button>
     </main>
   )
