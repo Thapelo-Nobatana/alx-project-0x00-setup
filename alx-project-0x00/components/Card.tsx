894db2e2f70a6ea1f3750dda0072a3ad8a0d30e3
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-07-20 12:56:13 +0200

    refactor: add type support for Pill component using PillProps interface

--- alx-project-0x00/components/Card.tsx
+++ alx-project-0x00/components/Card.tsx
@@ -7,13 +7,13 @@ import Pill from "./Pill"
 
 const Card: React.FC = () => {
     return (
-            <div className="h-[422px] w-[378.56px] cursor-pointer hover:shadow-md hover:rounded-lg ">
-      <Image className="rounded-lg" src={HOUSE_IMAGE} width={378.56} height={299.37} alt="house image" />
-      <div className="p-2 flex gap-2 mt-2">
-        <Pill title="Top Villa" />
-        <Pill title="Self CheckIn" />
-        <Pill title="Free Reschedule" />
-      </div>
+     <div className="h-[422px] w-[378.56px] cursor-pointer hover:shadow-md hover:rounded-lg ">
+        <Image className="rounded-lg" src={HOUSE_IMAGE} width={378.56} height={299.37} alt="house image" />
+        <div className="p-2 flex gap-2 mt-2">
+             <Pill title="Top Villa" />
+             <Pill title="Self CheckIn" />
+             <Pill title="Free Reschedule" />
+       </div>
       <div className="flex items-center justify-between">
         <div>
           <h3 className=" font-semibold text-[22px]">Villa Arrecife Beach House</h3>
