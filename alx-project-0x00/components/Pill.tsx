be2f9299dab63d9362ac05efa62adce3864c6a48
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-07-20 10:17:53 +0200

    feat: add Card and Pill components with basic layout and styles

---
+++ alx-project-0x00/components/Pill.tsx
@@ -0,0 +1,13 @@
+
+
+const Pill: React.FC = () => {
+    return (
+        <div className="flex judtify-center items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+
+            <p className="text-sm">Title</p>
+
+        </div>
+    )
+}
+
+export default Pill;
