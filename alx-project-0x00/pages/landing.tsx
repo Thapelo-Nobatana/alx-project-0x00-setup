295f25282f156bc43213a60f51ffb72842fed1c0
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-07-20 09:09:50 +0200

    feat: add basic routing with landing and about pages

---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,13 @@
+
+
+
+
+const Landing: React.FC =  () => {
+    return (
+        <div>
+            <h1 className="text-xl font-extralight">Landing Page</h1>
+        </div>
+    )
+}
+
+export default Landing;
